062a63cb36d5343ddb562a559c72cda6a505de17
andrei <andreitiltu@gmail.com>
2020-09-17 20:19:53 +0300

    test

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -5,11 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script
-            src="https://code.jquery.com/jquery-3.5.1.js"
-            integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-            crossorigin="anonymous"></script>
-    <script src="/public/js/main.js"></script>
     <title><?= $title ?></title>
 </head>
 <body>
