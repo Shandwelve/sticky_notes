c6639ab9ce97956e67d72ed5d8d89f52069d544c
andrei <andreitiltu@gmail.com>
2020-09-23 11:33:25 +0300

    bug fix

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -11,7 +11,6 @@ class MainController extends Controller
     public function index(): void
     {
         $notes = $this->model->getNotes();
-        $this->model->makeDate($notes);
         $this->view->render('Main Page', ['notes' => $notes]);
     }
 
